d5e13dda7218ba707aa03bb39fa2bf32057b44fa
ghofloyaragh <jkeyvan@gmail.com>
2023-08-15 10:06:43 +0330

    add signup url

--- cash_managemnet/urls.py
+++ cash_managemnet/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+from cash_managemnet.views import SignupView
+
+urlpatterns = [
+    path("signup", SignupView.as_view(), name="login")
+]
