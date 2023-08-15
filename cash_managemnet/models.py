30141473e2c18b6c7794af029b21ce273ceb05fd
ghofloyaragh <jkeyvan@gmail.com>
2023-08-15 16:30:25 +0330

    create cash_managemnet models

--- cash_managemnet/models.py
+++ cash_managemnet/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Transaction(models.Model):
+    class TypeChoices(models.TextChoices):
+        EXPENSE = ("E", "Expense")
+        INCOME = ("I", "Income")
+
+    amount = models.PositiveIntegerField(default=1)
+    type = models.CharField(max_length=1, choices=TypeChoices.choices)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
