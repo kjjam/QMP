e5acc629627977bb9753fa83817d513a2a6090e5
ghofloyaragh <jkeyvan@gmail.com>
2023-08-15 11:10:37 +0330

    changing views to accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.authtoken.admin import User
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from accounts.serilalizers import SignupSerializer
+
+
+class SignupView(CreateAPIView):
+    model = User
+    serializer_class = SignupSerializer
+
+
+class LogOutView(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def get(self, request):
+        request.user.auth_token.delete()
+        return Response({"username": request.user.username}, status=200)
