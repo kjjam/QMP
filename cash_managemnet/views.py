bcd92684543934746cbb1e3f7bfa71b61e3c6772
ghofloyaragh <jkeyvan@gmail.com>
2023-08-15 23:07:26 +0330

    adding crud transaction

---
+++ cash_managemnet/views.py
@@ -0,0 +1,27 @@
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from cash_managemnet.models import Transaction
+from cash_managemnet.serializers import TransactionSerializer
+
+
+class CreateTransactionView(CreateAPIView):
+    permission_classes = [IsAuthenticated, ]
+    model = Transaction
+    serializer_class = TransactionSerializer
+
+
+class UpdateTransactionView(UpdateAPIView):
+    permission_classes = [IsAuthenticated, ]
+    serializer_class = TransactionSerializer
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+
+
+class DeleteTransactionView(DestroyAPIView):
+    permission_classes = [IsAuthenticated, ]
+    model = Transaction
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
