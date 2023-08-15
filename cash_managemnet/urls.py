3cc64f4f5d309404b35e33a13e77eed73b4c656a
ghofloyaragh <jkeyvan@gmail.com>
2023-08-15 10:49:45 +0330

    adding login feature

--- cash_managemnet/urls.py
+++ cash_managemnet/urls.py
@@ -4,5 +4,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 from cash_managemnet.views import SignupView
 
 urlpatterns = [
-    path("signup", SignupView.as_view(), name="login")
+    path("signup", SignupView.as_view(), name="signup"),
+    path("login", obtain_auth_token, name="login")
 ]
