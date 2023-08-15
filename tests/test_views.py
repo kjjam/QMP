cf318646a254a9f094ae30daa7e1dc53aadea939
ghofloyaragh <jkeyvan@gmail.com>
2023-08-15 14:30:50 +0330

    change test urls

--- tests/test_views.py
+++ tests/test_views.py
@@ -11,14 +11,16 @@ class TestSignUpView(TestCase):
     def test_sign_up_successful_return_201(self):
         username = "keyvan"
         password = "123456"
-        response = self.client.post("/signup", data={"username": username, "password": password})
+        response = self.client.post("/accounts/signup", data={"username": username, "password": password},
+                                    content_type="application/json")
         self.assertEqual(response.status_code, 201, "Status code should be 201 !")
         self.assertEqual(response.data, {"username": "keyvan"}, "Response body should contain correct username only")
 
     def test_sign_up_duplicated_username_return_403(self):
         username = "keyvan"
         password = "123456"
-        self.client.post("/signup", data={"username": username, "password": password})
+        self.client.post("/accounts/signup", data={"username": username, "password": password}, content_type="application/json")
         duplicated_username = "keyvan"
-        response = self.client.post("/signup", data={"username": duplicated_username, "password": password})
+        response = self.client.post("/accounts/signup", data={"username": duplicated_username, "password": password},
+                                    content_type="application/json")
         self.assertEqual(response.status_code, 400, "Status code should be 400 !")
