b036d0fb6d9a62b9e952b46af9325711143c36e1
ghofloyaragh <jkeyvan@gmail.com>
2023-08-15 14:31:49 +0330

    change test urls

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework.authtoken.admin import User
 
 
@@ -11,7 +12,7 @@ class TestSignUpView(TestCase):
     def test_sign_up_successful_return_201(self):
         username = "keyvan"
         password = "123456"
-        response = self.client.post("/accounts/signup", data={"username": username, "password": password},
+        response = self.client.post(reverse("signup"), data={"username": username, "password": password},
                                     content_type="application/json")
         self.assertEqual(response.status_code, 201, "Status code should be 201 !")
         self.assertEqual(response.data, {"username": "keyvan"}, "Response body should contain correct username only")
@@ -19,8 +20,8 @@ class TestSignUpView(TestCase):
     def test_sign_up_duplicated_username_return_403(self):
         username = "keyvan"
         password = "123456"
-        self.client.post("/accounts/signup", data={"username": username, "password": password}, content_type="application/json")
+        self.client.post(reverse("signup"), data={"username": username, "password": password}, content_type="application/json")
         duplicated_username = "keyvan"
-        response = self.client.post("/accounts/signup", data={"username": duplicated_username, "password": password},
+        response = self.client.post(reverse("signup"), data={"username": duplicated_username, "password": password},
                                     content_type="application/json")
         self.assertEqual(response.status_code, 400, "Status code should be 400 !")
