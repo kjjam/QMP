dda5150c401f7208380343ef0a1171119193702f
ghofloyaragh <jkeyvan@gmail.com>
2023-08-15 11:07:39 +0330

    adding new accounts app

--- cash_managemnet/urls.py
+++ cash_managemnet/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from cash_managemnet.views import SignupView
+from cash_managemnet.views import SignupView, LogOutView
 
 urlpatterns = [
     path("signup", SignupView.as_view(), name="signup"),
-    path("login", obtain_auth_token, name="login")
+    path("login", obtain_auth_token, name="login"),
+    path("logout", LogOutView.as_view(), name="logout")
 ]
