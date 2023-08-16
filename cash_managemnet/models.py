ec18c6ef7dc97c54f3ff0b08666f90c1e2124a1c
ghofloyaragh <jkeyvan@gmail.com>
2023-08-16 11:37:27 +0330

    adding balance model

--- cash_managemnet/models.py
+++ cash_managemnet/models.py
@@ -6,10 +6,16 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
 
 
+class Balance(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amnt = models.IntegerField(default=0)
+
+
 class Transaction(models.Model):
     class TypeChoices(models.TextChoices):
         EXPENSE = ("E", "Expense")
         INCOME = ("I", "Income")
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.PositiveIntegerField(default=1)
     type = models.CharField(max_length=1, choices=TypeChoices.choices)
@@ -24,3 +30,6 @@ class Transaction(models.Model):
         ('date__lt', 'date__lt'),
         ('date_gt', 'date__gt')
     ]
+
+    def save(self, *args, **kwargs):
+        super(Transaction, self).save(*args, **kwargs)
